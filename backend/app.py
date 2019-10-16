d3d1b13f7e7317b4f21112ff1900452b262225b7
jkim393 <jkim393@ucr.edu>
2019-10-16 16:44:53 -0700

    flask connected to mongo via pymongo

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,25 @@
 from flask import Flask
-
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 
+cluster = MongoClient("mongodb+srv://adiach1:1234@cluster0-jgwg7.mongodb.net/test?retryWrites=true&w=majority")
+
+db = cluster["test"]
+collection = db["test"]
+
+post = {"_id": 4, "name":"Aditya", "score":1}
+
+collection.insert_one(post)
 
 @app.route("/", methods=['GET', 'POST'])
 def main():
-
+	
 	return 'Hi JiHwan'
 
 
-if __name__ == "__main__"
+if __name__ == "__main__":
 	app.run(debug=True)
-
 
 
