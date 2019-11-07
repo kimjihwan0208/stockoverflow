b3465b621176e4bee68cee67b59453b3528151a1
ericong18 <ericong18@gmail.com>
2019-11-07 00:25:27 -0800

    Clean up graph

--- frontend/src/components/Graph/Graph.js
+++ frontend/src/components/Graph/Graph.js
@@ -3,74 +3,41 @@ import { Line } from 'react-chartjs-2';
 import { setOptions } from '../../constants';
 import './Graph.css';
 
-// const  dataSet =
-// {
-//               labels: ["Jun-06", "Jun-07", "Jun-08", "Jun-09", "Jun-10", "Jun-11", "Jun-12", "Jun-13"],
-//               datasets: [
-//                 {
-//                   label:'Device_1',
-//                   data:[1, 4, 8, 2, 20, 23, 14, 9],
-//                   backgroundColor:'rgba(63, 63, 191, 0.6)'
-//                 },
-//                 {
-//                   label:'Device_2',
-//                   data:[23, 17, 30, 5, 3, 2, 13, 15],
-//                   backgroundColor: 'rgba(255, 35, 35, 0.6)'
-//                 }
-//               ]
-//         }
-
-// const dataSet = {
-//   labels: ["9:30", "10:30", "11:30", "12:30", "13:30", "14:30", "15:30"]
-//   datasets: [
-//     label: "current"
-//     backgroundColor:'rgba(63, 63, 191, 0.6)'
-//   ]
-// }
-
 function Graph(props) {
   const { dataPoints, openingStock, closingStock } = props;
 
-    const convert = () => {
-      let temp1 = []
-      let temp2 = []
-      let temp3 = []
-      let time = []
+  const convert = () => {
+    const currentPrice = dataPoints.map(dataPoint => dataPoint.y);
+    const opening = dataPoints.map(() => openingStock);
+    const closing = dataPoints.map(() => closingStock);
+    const time = dataPoints.map(dataPoint => dataPoint.t);
 
-      for (let i = 0; i < dataPoints.length; ++i){
-        time.push(dataPoints[i].t)
-        temp1.push(dataPoints[i].y)
-        temp2.push(openingStock)
-        temp3.push(closingStock)
+    const dataSet = {
+      labels: time,
+      datasets: [{
+        label: "Current Price",
+        data: currentPrice,
+        backgroundColor:'#F4F8FE',
+        borderColor: '#4963EF',
+      },
+      {
+        label: "Opening",
+        fill: false,
+        data: opening,
+        backgroundColor: 'rgba(255, 35, 35, 0.6)'
+      },
+      {
+        label: "Closing",
+        fill: false,
+        data: closing,
+        backgroundColor: 'rgba(255, 35, 35, 0.6)'
       }
-
-      let dataSet = {
-        labels: time,
-        datasets: [{
-          label: "Stock1",
-          fill: false,
-          data: temp1,
-          backgroundColor:'rgba(63, 63, 191, 0.6)'
-        },
-        {
-          label: "Opening",
-          fill: false,
-          data: temp2,
-          backgroundColor: 'rgba(255, 35, 35, 0.6)'
-        },
-        {
-          label: "Closing",
-          fill: false,
-          data: temp3,
-          backgroundColor: 'rgba(255, 35, 35, 0.6)'
-        }
-        ]
-      }
+      ]
+    }
 
     return dataSet
   }
 
-  console.log(openingStock)
   return (
     <div className="graph__container">
       <div className="graph__innerContainer">
