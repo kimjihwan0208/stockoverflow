fb7b2af15c9e8ce3f49deca41aa0c0d7a1e8b16e
Neal Goyal <ngoya001@ucr.edu>
2019-11-12 21:39:06 -0800

    Scraping multiple pages

--- backend/reuters_scraper.py
+++ backend/reuters_scraper.py
@@ -2,51 +2,38 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = 'http://www.reuters.com/news/archive/worldNews?view=page'
-result = requests.get(url)
-content = result.content
-content.decode().strip().replace('\t','').split('\n')
+baseurl = 'http://www.reuters.com/news/archive/worldNews?view=page='
+valid_pages = 'abcdefghijlmnoprstuvw'
+total_urls = []
+for n in range(1,10):
+    total_urls.append(baseurl + format(n))
+print(total_urls)
 
-# EXTRACTING CONTENT
-soup = BeautifulSoup(content, 'html.parser')
-soup.find_all('h3', {'class':'story-title'})
-
-# TITLES
 titles = []
-for title in soup.find_all('h3', {'class':'story-title'}):
-    titles.append(title.string.strip())
-
-# SUMMARY
 summary = []
-try:
-    for summary in soup.find_all('p'):
-        summary.append(summary.string.strip())
-except:
-    pass
-
-# DATE/TIME WRITTEN
 timestamp = []
-for written_time in soup.find_all('span', {'class':'timestamp'}):
-    timestamp.append(written_time.string.strip())
 
 
-#GETTING URL
+for url in total_urls:
+    result = requests.get(url)
+    content = result.content
+    content.decode().strip().replace('\t','').split('\n')
 
-# urls = []
-# import re
-# for tags in soup.find_all('a'):
-#    if re.search('article', tags['href']):
-#        urls.append(tags['href'])
-# urls = urls[:-5]
-# final_urls = []
-# for url in urls:
-#    if url not in final_urls:
-#        final_urls.append(url)
+    soup = BeautifulSoup(content, 'html.parser')
+    soup.find_all('h3', {'class':'story-title'})
 
+    for title in soup.find_all('h3', {'class':'story-title'}):
+        titles.append(title.string.strip())
 
-# Testing
+    try:
+        for summary in soup.find_all('p'):
+            summary.append(summary.string.strip())
+    except:
+        pass
+
+    for written_time in soup.find_all('span', {'class':'timestamp'}):
+        timestamp.append(written_time.string.strip())
 
 print(titles)
 print(summary)
 print(timestamp)
-#print(final_urls)
